0218b0fb1e0439f4110a1501164fc5ef9395c33d
eduin19950 <mugisha.edwin19950@gmail.com>
2022-09-08 14:48:17 -0700

    ff

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+
+*main -print the string in the put fuction
+*
+*Description: using the main function
+*this program prints 'Programming is like building a multilingual puzzle'
+*Returns:0
+*/
+int main(void)
+
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
